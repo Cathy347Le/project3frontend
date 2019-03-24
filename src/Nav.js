5956c6e8cbf89925b4ef99bb02635ce6ba3f4f00
Cathy <Cathy347Le@gmail.com>
2019-03-24 00:37:30 -0400

    got nav-bar to render using js instead of jquery

--- src/Nav.js
+++ src/Nav.js
@@ -12,7 +12,7 @@ class Nav extends Component {
             <a href="/" className="brand-logo">
               <img src={petzlogo} alt="logo" className="logo" />
             </a>
-            <a href="/" className="sidenav-trigger" data-target="mobile-nav">
+            <a href="#" data-target="slide-out" className="sidenav-trigger">
               <i className="material-icons">menu</i>
             </a>
             <ul className="right hide-on-med-and-down">
@@ -62,52 +62,70 @@ class Nav extends Component {
           </div>
         </nav>
 
-        <ul className="sidenav" id="mobile-nav">
+        <ul id="slide-out" className="sidenav blue lighten-3">
           {this.props.isLoggedIn === true && (
             <li>
-              <Link to="/" className="sidenav-close">
+              <Link to="/" className="sidenav-close white-text text-darken-2">
                 Home
               </Link>
             </li>
           )}
           {this.props.isLoggedIn === true && (
             <li>
-              <Link to="/pets" className="sidenav-close">
+              <Link
+                to="/pets"
+                className="sidenav-close white-text text-darken-2"
+              >
                 Pets
               </Link>
             </li>
           )}
           {this.props.isLoggedIn === true && (
             <li>
-              <Link to="/new" className="sidenav-close">
+              <Link
+                to="/new"
+                className="sidenav-close white-text text-darken-2"
+              >
                 New
               </Link>
             </li>
           )}
           {this.props.isLoggedIn === true && (
             <li>
-              <Link to="/search" className="sidenav-close">
+              <Link
+                to="/search"
+                className="sidenav-close white-text text-darken-2"
+              >
                 Search
               </Link>
             </li>
           )}
           {this.props.isLoggedIn === true && (
             <li>
-              <Link to="/about" className="sidenav-close">
+              <Link
+                to="/about"
+                className="sidenav-close white-text text-darken-2"
+              >
                 About
               </Link>
             </li>
           )}
           {this.props.isLoggedIn === false && (
             <li>
-              <Link to="/login" className="sidenav-close">
+              <Link
+                to="/login"
+                className="sidenav-close white-text text-darken-2"
+              >
                 Login
               </Link>
             </li>
           )}
           {this.props.isLoggedIn === false && (
             <li>
-              <Link to="/signup" className="sidenav-close">
+              <Link
+                to="/signup"
+                className="sidenav-close white-text text-darken-2"
+              >
                 Sign Up
               </Link>
             </li>
@@ -117,7 +135,7 @@ class Nav extends Component {
               <Link
                 to="/logout"
                 onClick={this.props.handleLogOut}
-                className="sidenav-close"
+                className="sidenav-close white-text text-darken-2"
               >
                 Logout
               </Link>
